d6be6b2cadf0f80a7f13ae0b3fd4f39b7a195f46
Ben <burge337@morris.umn.edu>
2020-09-24 13:14:58 -0500

    Commiting the copies we made of mergesort.c and .h for our array_merge function.
    Forgot these in the prior commit. We need these because array_merge uses mergesort.

---
+++ array_merge/mergesort.c
@@ -0,0 +1,57 @@
+#include "mergesort.h"
+
+void mergesort(int size, int *values) {
+	mergesortRange(values, 0, size);
+}
+
+void mergesortRange(int *values, int startIndex, int endIndex) {
+	int rangeSize = endIndex - startIndex;
+	if (needsSorting(rangeSize)){
+		int midPoint = (startIndex + endIndex) / 2;
+		mergesortRange(values, startIndex, midPoint);
+		mergesortRange(values, midPoint, endIndex);
+		mergeRanges(values, startIndex, midPoint, endIndex);
+	}
+}
+
+
+void mergeRanges(int *values, int startIndex, int midPoint, int endIndex) {
+	int rangeSize = endIndex - startIndex;
+	// need to make sure to free destination array at some point
+	int *destination = (int*)calloc(rangeSize, sizeof(int));
+	int firstIndex = startIndex;
+	int secondIndex = midPoint;
+	int copyIndex = 0;
+	while (firstIndex < midPoint && secondIndex < endIndex) {
+		if (values[firstIndex] < values[secondIndex]) {
+			destination[copyIndex] = values[firstIndex];
+			++firstIndex;
+		} else {
+			destination[copyIndex] = values[secondIndex];
+			++secondIndex;
+		}
+		++copyIndex;
+	}
+	while (firstIndex < midPoint) {
+		destination[copyIndex] = values[firstIndex];
+		++copyIndex;
+		++firstIndex;
+	}
+	while (secondIndex < endIndex) {
+		destination[copyIndex] = values[secondIndex];
+		++copyIndex;
+		++secondIndex;
+	}
+	for (int i = 0; i < rangeSize; i++) {
+		values[i + startIndex] = destination[i];
+	}
+	free(destination);
+}
+
+bool needsSorting(int rangeSize) {
+	return rangeSize >= 2;
+}
+
+
+
+
