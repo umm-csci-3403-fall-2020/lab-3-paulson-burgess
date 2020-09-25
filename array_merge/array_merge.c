2a97463b961227f45695cc645e60df474f860f2e
Ben <burge337@morris.umn.edu>
2020-09-24 19:44:11 -0500

    Fix most of the tests for array_merge.
    We've seemingly fixed some of the array indices issues, and have actually captured a zero if any of the values in
    our values array contained any. (before our comparison to check if we were adding unique values always failed with
    zero because malloc and calloc seem to initialize arrays with zeros)
    
    Only the final test: Handle_different_sizes_reversed is failing, where the 0th element (k or length of array)
    in our result is 4 where it should be 10. No idea what's going on with that.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,60 +1,85 @@
 #include "array_merge.h"
 #include "mergesort.h"
 
+// function that generates a single sorted list of the unique values in an array of arrays. (small -> large)
 int* array_merge(int num_arrays, int* sizes, int** values) {
   int total = 0; 
   
   for(int i = 0; i < num_arrays; ++i){
   total = total + sizes[i];
   }
-
-  int* temp = (int *) calloc(total, sizeof(int));
-  // counter for array of arrays (values)
+  // our array to hold the unique values in our values array (with extra memory allocated for safety)
+  int* temp = (int *) malloc(total* sizeof(int));
+  // counter to find interior array we want to iterate through
   int i = 0;
   // counter for our temp array
-  int n = 1;
+  int n = 0;
   // length of our merged array of unique values
   int k = 0;
-	
-// ex run-through total is 1
-
-  while(i != num_arrays){
   
+  // increment temp array by one (capturing a unique zero that otherwise would be looked over)
+  if(containsZero(values, num_arrays, sizes, temp)){
+	n++;
+	k++;
+  }
+  // while we haven't gone through all of the exterior arrays...
+  while(i != num_arrays){
+  	  // so long as there is still stuff to iterate through in the interior array...
 	  for(int j = 0; j < sizes[i]; ++j){
+		  // and our temp array doesn't already have the value stored in that index...
 		  if(!(isIn(values[i][j], temp, total))){
+		  // it's a unique value -> chuck it in our temp array.
 	  	  temp[n] = values[i][j];
+
+		  // we only increment temp stuff in our if statement so we don't get gaps in the array
 		  k++;
 		  n++;
 		  }
 	  }
+  // go to next exterior array
   i++;
   }
   // sort our temp array
-  mergesort(total,temp);
+  mergesort(k,temp);
 
   int* finalArray = resizeArray(temp, k);
   // make sure we also put the length in there
   finalArray[0] = k;
-  if (sizeof(temp) != 0){
-  free(temp);
-  }
   return finalArray;
 }
-	// case of value = 0, temp array passed, and size = total = 1 
+   // function that checks to see if the passed array already has the passed value in it
+   // returns true if it already has it and false if it doesn't 
    bool isIn(int value, int* array, int size){
-   // check to see if our temp array already has a given value
-   // with the exception f the 0th index which is reserved for k. (which can have one duplicate)
-   for(int i = 1; i < size; ++i)
+   for(int i = 0; i < size; ++i)
 	if(array[i] == value){
 	return true;
 	}
    return false;
    }
-
+   // copies the values in our temporary array into a resized array that only allocates exactly the memory it needs.
    int* resizeArray(int* array,int k){
-	   int* resizedArray = (int*) calloc(k + 1, sizeof(int));
-	   for(int i = 1; i < k + 1; i++) {
+	   int* resizedArray = (int*) malloc((k + 1)* sizeof(int));
+	   // starts copying at index 1 instead of 0 to leave room for k at the start.
+	   for(int i = 0; i < k; i++) {
 		   resizedArray[i + 1] = array[i];
 	   }
+	   // should be able to free the memory given to temp now
+	   free(array);
 	   return resizedArray;
    }
+
+   bool containsZero(int** values, int num_arrays, int* sizes, int* temp) {
+	int i = 0;
+	while(i < num_arrays){
+	  for(int j = 0; j < sizes[i]; ++j){
+		  // iterating through the array of arrays to see if there is a zero
+		  if(values[i][j] == 0){
+		  // only want the one zero
+		  return true;
+		  }
+	  }
+	  i++;
+	}
+	return false;
+   }
+	
--- array_merge/array_merge.h
+++ array_merge/array_merge.h
@@ -8,6 +8,7 @@ int* array_merge(int num_arrays, int* sizes, int** values);
 bool isIn(int value, int* array, int size);
 int* resizeArray(int* array, int k);
 void mergesort(int size, int *values);
+bool containsZero(int** values, int num_array, int* sizes, int* temp);
 
 
 #endif
